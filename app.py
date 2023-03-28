feda7ee5a1a6787c1fd1277de42695763bb2ff7b
Revanth3103 <68495515+Revanth3103@users.noreply.github.com>
2023-03-28 14:03:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ st.title('Forecasting')
 df = st.file_uploader('Import the time series csv file here. Columns must be labeled ds and y. The input to Prophet is always a dataframe with two columns: ds and y. The ds (datestamp) column be ideally YYYY-MM-DD for a date or YYYY-MM-DD HH:MM:SS for a timestamp. The y(target) column must be numeric, and represents the measurement we wish to forecast.', type='csv')
 st.write('CSV file can be updated and reuploaded any number of times inorder to get prediction and forecasting depends on only two factors date(ds) and target column(y)')
 
-use_defo = st.selectbox('Select Dataset',('Rice','Sugar','oil'))
+use_defo = st.selectbox('Select Dataset',('Rice','sugar','oil'))
 if use_defo == 'Rice':
     df = 'Riceprice.csv'
-elif use_defo == 'Sugar':
-    df = 'Sugar.csv'
+elif use_defo == 'sugar':
+    df = 'sugar.csv'
 else :
     df ='oil.csv'
 
